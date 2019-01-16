1aac6da0317fed9f4478991b40054cb6ed446496
licetus <licetus0316@gmail.com>
2019-01-16 18:03:01 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: [ 2, 'never' ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-confusing-arrow': 'off',
+    'max-len': 'off',
+    'global-require': 'off',
+    'no-tabs': ['off'],
+    'import/prefer-default-export': ['off'],
+    'object-curly-newline': ['off'],
+  },
+}
